3461793598b3272ebfb4bb81e41f17674f5db14d
OliverZeymer <u28282@rts-365.dk>
2023-12-28 13:43:40 +0100

    remove use search params from toplevel client to fix client side rendering

--- src/components/TopLevelClient.jsx
+++ src/components/TopLevelClient.jsx
@@ -1,15 +1,14 @@
 'use client';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import * as NProgress from 'nprogress';
 import { useEffect } from 'react';
 import ContextProvider from './ContextProvider';
 export default function TopLevelClient({ children }) {
   const router = useRouter();
   const pathname = usePathname();
-  const searchParams = useSearchParams()
   useEffect(() => {
     NProgress.done();
-  }, [pathname, router, searchParams]);
+  }, [pathname, router]);
 
   return <ContextProvider>{children}</ContextProvider>;
 }
