9dadaf27e36353fa4e13861aae618a160bbc4e7e
OliverZeymer <u28282@rts-365.dk>
2023-12-28 06:19:03 +0100

    remove autoplay on trailer

--- src/components/AnimeTrailer.jsx
+++ src/components/AnimeTrailer.jsx
@@ -1,9 +1,10 @@
-export default function AnimeTrailer({trailer}) {
+export default function AnimeTrailer({ trailer }) {
+  const trailerWithoutAutoplay = trailer?.replace('&autoplay=1', '&autoplay=0');
   return (
     <div className='bg-primary-foreground p-4 rounded-2xl h-fit'>
       <h3 className='text-xl lg:text-2xl font-bold'>Trailer</h3>
       <iframe
-        src={trailer + '&autoplay=0'}
+        src={trailerWithoutAutoplay}
         title='YouTube video player'
         className='aspect-video w-full rounded-2xl mt-4 max-w-[2000px]'
         allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture;'
