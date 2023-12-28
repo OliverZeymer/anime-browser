1daf158d80e8ce43a6f75035701bb2a76c05aa61
OliverZeymer <u28282@rts-365.dk>
2023-12-28 06:01:01 +0100

    fix build error

--- src/components/NoResultsFound.jsx
+++ src/components/NoResultsFound.jsx
@@ -7,7 +7,7 @@ export default function NoResultsFound() {
     <div className='flex flex-col items-center gap-4'>
       <Image src='/images/tobi.png' alt='Tobi' className='mb-6' width={500} height={500} />
       <h2 className='text-xl md:text-2xl lg:text-3xl font-bold'>No Results Found</h2>
-      <p>We sadly couldn't find any results. Please try again later.</p>
+      <p>We sadly couldn&apos;t find any results. Please try again later.</p>
       <Button asChild>
         <Link href='/'>Back to home</Link>
       </Button>
