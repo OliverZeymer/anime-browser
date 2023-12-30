3e4b1bbac01aa0daa6bd10d671fc37ab2e88a80f
OliverZeymer <u28282@rts-365.dk>
2023-12-30 07:04:57 +0100

    grid styling fix - for now

--- src/components/AnimeCardList.jsx
+++ src/components/AnimeCardList.jsx
@@ -6,7 +6,7 @@ import { cn } from '@/lib/utils';
 export default function AnimeCardList({ data, isRecommendations, isCharacter, className }) {
   return (
     <section className={!isCharacter && 'mt-6'}>
-      <ul className={cn('sm:grid sm:grid-cols-auto-fit flex flex-col items-center sm:items-start place-items-center gap-3 lg:gap-6 w-full h-fit', className)}>
+      <ul className={cn('sm:grid sm:grid-cols-auto-fit flex flex-col items-center sm:items-start gap-3 lg:gap-6 w-full h-fit', className)}>
         {data?.length === 0 || !data ? (
           <NoResultsFound />
         ) : (
